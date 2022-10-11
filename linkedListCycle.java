4cd5e44f809f722f60351835819eb0fdf3863bfb
Avinash Chourasia <97829396+AvinashChourasia@users.noreply.github.com>
2022-10-11 20:25:58 +0530

    Create linkedListCycle.java

---
+++ linkedListCycle.java
@@ -0,0 +1,16 @@
+// Detect a cycle in the Linked List (problem of leetcode)
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
