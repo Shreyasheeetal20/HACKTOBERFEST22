9ef2916b4f3bdcc10b5656c26bd3d71fa41d82d0
Arindam Das <67632565+arindamdas123@users.noreply.github.com>
2022-10-24 17:17:56 +0530

    Create reference_variable.java

---
+++ reference_variable.java
@@ -0,0 +1,16 @@
+class sample
+{
+	void add()
+	{
+		int a=20;
+		int b=30;
+		int c=a+b;
+		System.out.println(c);
+	}
+public static void main(String[] args) 
+	{
+		sample s1=new sample();
+		s1.add();
+		s1.add();
+	}
+}
