60ec12867ecd33bd4462c483ec34090e62e7c983
Vineet Kumar <vkrmgs69@gmsil.com>
2022-10-28 12:57:32 +0530

    Another method for alpha pattern

--- AlphaPattern.cpp
+++ AlphaPattern.cpp
@@ -1,15 +1,30 @@
+/*A
+  BC
+  CDE
+  DEFG*/
+
 #include<iostream>
 using namespace std;
-
-
-int main(){
-    
-    int n;
-    cin >> n;
-    for(int i = 1; i<=n;i++){
-        for(int j = 1; j<=i;j++)
-            cout << char(i+'A'-1);
-        cout << endl;
+int main()
+{
+  int n;
+  cin>>n;
+  int i=1;
+ 
+  while (i<=n)
+   
+  {
+    int j=1;
+    char ch='A'+i-1;
+    while(j<=i){
+    //ch='A'+i;
+    cout<<ch;
+    ch++;
+    j++;
     }
-    
+  
+  cout<<endl;
+  i++;
+  }
+ return 0;
 }
